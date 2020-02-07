8b20390ba8986282913c88d8436bbc8e36f08808
Jen Glaubius <glaubius@gmail.com>
2020-02-07 11:22:24 -0500

    add nav menus to footer

--- footer.php
+++ footer.php
@@ -17,27 +17,78 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <div class="wrapper" id="wrapper-footer">
 
-	<div class="<?php echo esc_attr( $container ); ?>">
+	<div class="container-fluid">
 
-		<div class="row">
+		<div class="row p-4">
 
-			<div class="col-md-12">
-
-				<footer class="site-footer" id="colophon">
-
-					<div class="site-info">
-
-						<?php understrap_site_info(); ?>
-
-					</div><!-- .site-info -->
-
-				</footer><!-- #colophon -->
-
+			<div class="col-md-4">
+				<!-- empty for spacing -->
 			</div><!--col end -->
+			<div class="col-md-4 text-center">
+				<a href="https://helonaki.com"><i class="fas fa-home"></i></a>
+			</div>
 
 		</div><!-- row end -->
 
 	</div><!-- container end -->
+	<div class="<?php echo get_attr( $container ); ?> ">
+		<div class="container-fluid">
+			<div class="row p-4">
+				<div class="col-md-3">
+					<a href="https://helonaki.com">ADD HELONAKI LOGO HERE</a>
+					<p>Copyright 2020 The Helonaki <br>All rights reserved. </p>
+				</div>
+				<div class="col-md-3">
+					<h4 class="headline headline--small">Resources</h4>
+					<nav>
+						<?php wp_nav_menu(
+							array(
+								'theme_location'  => 'footerMenu1',
+								// 'container_class' => 'nav flex-column',
+								'menu_class'      => 'nav flex-column',
+								'fallback_cb'     => '',
+								'menu_id'         => 'footerMenu',
+								'depth'           => 2,
+								'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
+							)
+						); ?>
+					</nav>
+				</div>
+				<div class="col-md-3">
+					<h4 class="headline headline--small">Resources</h4>
+					<nav>
+						<?php wp_nav_menu(
+							array(
+								'theme_location'  => 'footerMenu2',
+								// 'container_class' => 'nav flex-column',
+								'menu_class'      => 'nav flex-column',
+								'fallback_cb'     => '',
+								'menu_id'         => 'footerMenu',
+								'depth'           => 2,
+								'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
+							)
+						); ?>
+					</nav>
+				</div>
+				<div class="col-md-2">
+					<h4 class="headline headline--small text-center">Connect with The Helonaki</h4>
+					<nav>
+						<?php wp_nav_menu(
+							array(
+								'theme_location'  => 'socialMediaMenu',
+								// 'container_class' => 'nav flex-column',
+								'menu_class'      => 'nav flex-column',
+								'fallback_cb'     => '',
+								'menu_id'         => 'footerMenu',
+								'depth'           => 2,
+								'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
+							)
+						); ?>
+          </nav>
+				</div>
+			</div>
+		</div>
+	</div>
 
 </div><!-- wrapper end -->
 
@@ -48,4 +99,3 @@ $container = get_theme_mod( 'understrap_container_type' );
 </body>
 
 </html>
-
