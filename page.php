636e76913cd4338b56ed11177fee0b5f3ee5490f
Jen Glaubius <glaubius@gmail.com>
2020-02-11 09:18:36 -0500

    add code for hero slider

--- page.php
+++ page.php
@@ -19,6 +19,10 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 ?>
 
+<?php if ( is_front_page() && is_home() ) : ?>
+	<?php get_template_part( 'global-templates/hero' ); ?>
+<?php endif; ?>
+
 <div class="wrapper" id="page-wrapper">
 
 	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
